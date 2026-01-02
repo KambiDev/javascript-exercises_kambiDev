97043a8e979c7260e2aad210cb0a4dc7fb98b003
kambises Elias <kambiseselias89@gmail.com>
2026-01-02 11:45:34 -0600

    finish/11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return Math.round((f - 32) * 5 / 9, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return Math.round((c * 1.8) + 32, 1);
 };
 
 // Do not edit below this line
