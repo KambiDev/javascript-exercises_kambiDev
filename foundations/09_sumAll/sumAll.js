53ebb25baf30f335acbe23294c0a406ea6a96799
kambises Elias <kambiseselias89@gmail.com>
2026-01-02 00:39:45 -0600

    finish exercise/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    while (min <= max) {
+        sum += min;
+        min++;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
