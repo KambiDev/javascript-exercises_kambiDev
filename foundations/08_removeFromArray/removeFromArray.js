5cc416fc74a3e6aedc40bd42917bf4f17b295846
kambises Elias <kambiseselias89@gmail.com>
2026-01-02 00:26:56 -0600

    finish exercise/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+    
+    array.forEach((element, index) => {
+        if (element === num){
+            array.splice(index, 1);
+        }
+    });
+
+    return array;
 };
 
 // Do not edit below this line
