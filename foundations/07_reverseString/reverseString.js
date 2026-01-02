a4e11a980ad10d3c439b506a66da2a16fbe69561
kambises Elias <kambiseselias89@gmail.com>
2026-01-02 00:10:13 -0600

    finish exercise/07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let reverseString = '';
+
+    for (let index = str.length - 1; index >= 0; index--) {
+        
+        reverseString += str[index];
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
