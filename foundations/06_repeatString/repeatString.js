e1dd00697af64c5c39d06b7c151c9e61e535a520
kambises Elias <kambiseselias89@gmail.com>
2026-01-01 21:45:09 -0600

    finish exercise/06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, nVeces) {
 
+    if (nVeces < 0){return 'Error';}
+
+    let cadena = '';
+    
+    for (let index = 0; index < nVeces; index++) {
+        cadena += str;
+    }
+
+    return cadena;
 };
 
 // Do not edit below this line
