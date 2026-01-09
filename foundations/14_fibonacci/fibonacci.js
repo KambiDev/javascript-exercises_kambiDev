60f8e4a9fc0dc995cb40fb478d23c7edb01ce240
kambises Elias <kambiseselias89@gmail.com>
2026-01-09 00:54:49 -0600

    finish exercise/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(numero) {
 
+    const fibo = [0, 1];
+
+    for (let i = 2; i <= numero; i++){ 
+
+        fibo.push(fibo[i - 2] + fibo[i - 1]);
+
+    }
+
+    return fibo[numero];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
