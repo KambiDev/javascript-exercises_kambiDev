c439ba4f1e7f1011d266b932d9d7f792e2092044
kambises Elias <kambiseselias89@gmail.com>
2026-01-09 00:21:39 -0600

    finish exercise/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const limpiarString = string.split(' ').join('').toLowerCase();
+    
+    const stringInvertido = limpiarString.split('').reverse().join('');
 
+    const palindromo = limpiarString === stringInvertido;
+
+    return palindromo;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
