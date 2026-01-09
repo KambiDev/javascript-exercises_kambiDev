ec86efe9b96ba0f77177c55bd2d94a5b3f87daf7
kambises Elias <kambiseselias89@gmail.com>
2026-01-09 11:59:09 -0600

    finish exercise/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(personas) {
+
+    return personas.reduce((mayor, persona) => {
+
+        const anioActual = new Date().getFullYear();
+
+        const muertePersona = persona.fallecimiento || anioActual;
+        const muerteMayor = mayor.fallecimiento || anioActual;
+
+        const edadPersona = muertePersona - persona.nacimiento;
+        const edadMayor = muerteMayor - mayor.nacimiento;
+
+        if (edadPersona > edadMayor){
+            return persona;
+        }
+
+        return mayor;
+    });
 
 };
 
