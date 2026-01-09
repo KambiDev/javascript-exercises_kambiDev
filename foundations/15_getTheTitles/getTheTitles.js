b4a59477d3e477c9b82dde8ab17c58aefb029553
kambises Elias <kambiseselias89@gmail.com>
2026-01-09 01:03:06 -0600

    finish exercise/15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(libros) {
 
+    return libros.map(libro => libro.title);
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
