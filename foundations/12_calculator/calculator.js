571851a7ec2053da58fa2fcefdba4e22c09ef48f
kambises Elias <kambiseselias89@gmail.com>
2026-01-08 22:10:50 -0600

    finish exercise/12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acumulador, numero) => acumulador + numero, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acumulador, numero) => acumulador * numero, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponente) {
+  let acc = 1;
+  for (let index = 0; index < exponente; index++) {
+    acc *= base;
+  }
+  return acc;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact =  1;
+  for (let index = 1; index <= num; index++) {
+    fact *= index;
+  }
+  return fact;
 };
 
 // Do not edit below this line
